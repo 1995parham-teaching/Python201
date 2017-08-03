d766fef408579af033d3696e78b7b48f156f5ecb
Parham Alvani <parham.alvani@gmail.com>
2017-08-03 22:44:38 +0430

    Hello sample was added.

---
+++ src/hello.py
@@ -0,0 +1,10 @@
+# In The Name Of God
+# ========================================
+# [] File Name : hello.py
+#
+# [] Creation Date : 03-08-2017
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+if __name__ == '__main__':
+    print('hello world')
