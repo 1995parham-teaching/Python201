50bd3cd69eff859f327ad64c4a977fd0fe3b0dd9
Parham Alvani <parham.alvani@gmail.com>
2017-08-05 16:01:04 +0430

    Function citizen example was added.

---
+++ src/function-citizen.py
@@ -0,0 +1,19 @@
+# In The Name Of God
+# ========================================
+# [] File Name : function-citizen.py
+#
+# [] Creation Date : 05-08-2017
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+
+
+def a_hello():
+    print("Hi I am function A")
+
+
+a_hello()
+print(a_hello)
+b_hello = a_hello
+b_hello()
+1()
