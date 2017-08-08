757f73b63112fe19deeca5c23b3e9c0c9b5509d8
Parham Alvani <parham.alvani@gmail.com>
2017-08-09 01:56:18 +0430

    Adds class and set class examples.

---
+++ src/class-c.py
@@ -0,0 +1,9 @@
+class Person():
+    'this is person'
+
+    def say(self):
+        print("hello")
+
+
+p = Person()
+p.say()
---
+++ src/set-c.py
@@ -0,0 +1,6 @@
+a = set([1])
+print(a)
+b = {1, 2}
+print(b)
+print(a | b)
+print(a & b)
